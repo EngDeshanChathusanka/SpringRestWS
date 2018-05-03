800a1d0a0faa447260ae23fb9d124257353baf1e
Deshan <deshan@atlassian.com>
2018-05-03 22:29:09 +0530

    mock private method

--- src/test/PersonServiceTest.java
+++ src/test/PersonServiceTest.java
@@ -2,6 +2,9 @@ import com.rest.Person;
 import com.rest.component.PersonService;
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import static org.junit.Assert.assertEquals;
 
 public class PersonServiceTest {
@@ -11,5 +14,17 @@ public class PersonServiceTest {
         PersonService personService = new PersonService();
         Person person = personService.getPerson(15);
         assertEquals(15, person.getId());
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void privateCountLetters() throws NoSuchMethodException,
+            InvocationTargetException, IllegalAccessException {
+        PersonService personService = new PersonService();
+
+        Method method = PersonService.class.getDeclaredMethod("getInstance");
+        method.setAccessible(true);
+        Person person = (Person) method.invoke(personService);
+
     }
 }
